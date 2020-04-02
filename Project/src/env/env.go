186b65fa32f68e62d58122a81cab236d5608469e
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-04-02 07:38:43 -0300

    Added a basic authentication and env package

---
+++ Project/src/env/env.go
@@ -0,0 +1,9 @@
+package env
+
+import "os"
+
+//New start function for configure all environment constants
+func New() {
+	os.Setenv("BASIC_USERNAME", "test")
+	os.Setenv("BASIC_PASSWORD", "test")
+}
--- Project/src/main.go
+++ Project/src/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 	"time"
+
+	"github.com/zangeronimo/authenticated/env"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -12,11 +17,29 @@ import (
 const SAMPLESECRET string = "Hello JWT"
 
 func main() {
-	http.HandleFunc("/auth", helloServer)
+	env.New()
+
+	http.HandleFunc("/auth", basicAuth)
 	http.ListenAndServe(":4001", nil)
 }
 
-func helloServer(w http.ResponseWriter, r *http.Request) {
+func basicAuth(w http.ResponseWriter, r *http.Request) {
+	// Check basic login for basic authentication, first stap to generate a JWT
+	auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+
+	if len(auth) != 2 || auth[0] != "Basic" {
+		http.Error(w, "authorization failed", http.StatusUnauthorized)
+		return
+	}
+
+	payload, _ := base64.StdEncoding.DecodeString(auth[1])
+	pair := strings.SplitN(string(payload), ":", 2)
+
+	if len(pair) != 2 || !validate(pair[0], pair[1]) {
+		http.Error(w, "authorization failed", http.StatusUnauthorized)
+		return
+	}
+
 	// Create a new token object, specifying signing method and the claims
 	// you would like it to contain.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
@@ -29,4 +52,15 @@ func helloServer(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(tokenString, err)
 	fmt.Fprintf(w, "Hello, %s - Err %s!", tokenString, err)
+}
+
+func validate(username, password string) bool {
+
+	basicUsername := os.Getenv("BASIC_USERNAME")
+	basicPassword := os.Getenv("BASIC_PASSWORD")
+
+	if username == basicUsername && password == basicPassword {
+		return true
+	}
+	return false
 }
